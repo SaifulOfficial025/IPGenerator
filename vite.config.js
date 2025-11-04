4dad2c2e7c58fcb6231027cb8ed976ac23bb4fb5
Md Saiful Islam Rimon <saifulofficial025@gmail.com>
2025-11-04 13:48:37 +0600

    base url has changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 6969, // Moved port to the server level
     proxy: {
       '/api': {
-        target: 'https://ethan-project.duckdns.org/',
+        // target: 'https://ethan-project.duckdns.org/',
+        target: 'https://ethan10.duckdns.org/',
         changeOrigin: true, // Enable to handle CORS by changing the origin
         secure: false, // Disable SSL verification for ngrok
         ws: false // Disable WebSocket proxying if not needed
